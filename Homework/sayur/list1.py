82e32c73053febb36077b2b9f7cda2aa5023855c
priya211003 <priyamahalakshmiselvamurugan@gmail.com>
2023-09-19 20:19:49 +0530

    HW

--- Homework/sayur/list1.py
+++ Homework/sayur/list1.py
@@ -1,11 +1,20 @@
 subjects = int(input("Enter the number of subjects: "))
 total_marks = 0
-failed_subjects = []
-for i in range(1, subjects + 1):
-    marks = int(input(f"Enter marks for subject {i}: "))
+subject_results = []
+for i in range(subjects):
+    subject_name = input(f"Enter the marks for subject {i + 1}: ")
+    marks = float(subject_name)
+    total_marks = total_marks + marks
     if marks < 35:
-        failed_subjects.append(f"Subject {i}")
-    total_marks += marks
-print(f"Total marks: {total_marks}")
-if failed_subjects:
-    print(f"You failed in the subject{i} with {marks} marks.")
+        subject_results.append("Fail")
+    else:
+        subject_results.append("Pass")
+average = total_marks / subjects
+for i in range(subjects):
+    print(f"Subject {i + 1}: {subject_results[i]}")
+print(f"Total Marks: {total_marks}")
+print(f"Average Marks: {average:.2f}")
+if "Fail" in subject_results:
+    print("Overall Result: Fail")
+else:
+    print("Overall Result: Pass")  
